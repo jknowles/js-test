96846d8f2fdbfac63390b3731b9b368e9e5274d2
Jared Knowles <jknowles@gmail.com>
2013-05-27 16:49:42 -0500

    Outline of files

---
+++ test.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Jared
+ * Date: 5/27/13
+ * Time: 4:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+print("Welcome.");
+var pointEstimate = prompt("What is your point estimate");
+var stdError = prompt("What is the standard error");
+console.log(pointEstimate + 2*stdError);
